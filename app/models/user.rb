991241771b3ecf8dcd992f0204d0c721c933ebd0
iii <ppr@gmail.com>
2023-03-21 14:31:10 +0530

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   validates :name, presence: true, length: { maximum: 50 }
   before_save :downcase_email
   before_create  :create_activation_digest
@@ -8,7 +8,7 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password,  length: { minimum: 6 }, allow_nil: true
+  validates :password,  length: { minimum: 6 }, allow_blank:  true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -57,6 +57,22 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   private
   # Converts email to all lower-case.
